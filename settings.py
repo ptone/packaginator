164fc51c0637fa61ac2045ac4d817e0018d1ef7a
ptone <preston@ptone.com>
2011-04-07 15:52:39 -0700

    updated deprecated context processor path

--- settings.py
+++ settings.py
@@ -129,11 +129,11 @@ TEMPLATE_DIRS = [
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = [
-    "django.core.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.request",
+    "django.contrib.auth.context_processors.auth",
     "django.contrib.messages.context_processors.messages",
     
     "staticfiles.context_processors.static_url",
